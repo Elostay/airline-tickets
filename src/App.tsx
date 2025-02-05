065e09f5ed292bca6394c658968eaeecaf93e688
Elostay <nastia60203@gmail.com>
2025-02-05 16:04:30 +0200

    add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
@@ -14,17 +19,19 @@ const theme = createTheme({
 
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <Routes>
-            <Route path="/" element={<FlightsPage />} />
-            <Route path="/flights/:id" element={<FlightDetailsPage />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </Provider>
+    <BrowserRouter basename="/airline-tickets">
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <Routes>
+              <Route path="/" element={<FlightsPage />} />
+              <Route path="/flights/:id" element={<FlightDetailsPage />} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
